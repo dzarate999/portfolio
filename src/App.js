9d5f72d7c7c2d3e18d83a665f16fb3b866c7ab5b
Daniel Zarate <daniel@zarate.org>
2019-10-22 15:01:08 -0500

    Removed App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import './App.css';
 import Nav from 'react-bootstrap/Nav';
 
 
